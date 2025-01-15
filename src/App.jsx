7c55dde92d50d3c24ca6ddf043c1af4edfe5e6c2
Henrick <ricknogueira1231@gmail.com>
2025-01-15 20:02:51 -0300

    #D007 Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import Post from "./Post";
+import {Post} from "./Post";
 
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Henrick Nogueira"
+        content="Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum"
+      />
+      <Post
+        author="Ana Claudia"
+        content="Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum"
+      />
     </div>
   );
 }
