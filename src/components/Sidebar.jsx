682edb571101b4dd5d7605357ba3b2233f250f0e
Henrick <ricknogueira1231@gmail.com>
2025-01-16 18:13:40 -0300

    #D012 - Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,26 @@
-import styles from "./Sidebar.module.css";
+import {PencilLine} from "@phosphor-icons/react";
 
+import styles from "./Sidebar.module.css";
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img className={styles.cover} src="https://images.unsplash.com/photo-1611647832580-377268dba7cb?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1611647832580-377268dba7cb?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
 
-    <div className={styles.profile}>
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/55353527?v=4" />
+
         <strong>Henrick Nogueira</strong>
         <span>Web Developer</span>
-    </div>
+      </div>
 
-    <footer>
-        <a href="#">Editar seu perfil</a>
-    </footer>
-
+      <footer>
+        <a href="#">
+          <PencilLine />
+          Editar seu perfil</a>
+      </footer>
     </aside>
   );
 }
