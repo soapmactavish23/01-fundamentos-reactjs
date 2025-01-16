6c4ade7c027527aebcde3a8f2d3d2edd3180ec41
Henrick <ricknogueira1231@gmail.com>
2025-01-16 17:58:47 -0300

    #D011 - Componente Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
 
 import "./global.css";
 
@@ -7,14 +10,15 @@ export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Henrick Nogueira"
-        content="Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum"
-      />
-      <Post
-        author="Ana Claudia"
-        content="Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Henrick Nogueira"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut eros eget velit convallis elementum. Sed id velit non massa condimentum ultricies."
+          />
+        </main>
+      </div>
     </div>
   );
 }
