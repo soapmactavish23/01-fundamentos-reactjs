da727d1f3b0bde11876a848da007b351ed7f50c7
Henrick <ricknogueira1231@gmail.com>
2025-01-17 16:01:04 -0300

    https://gitlab.prodesp.sp.gov.br/folha-centralizada/rh-folha/rhfolha-rho/gestao-acesso/menu-app/-/tree/feature/lab_new_front_end?ref_type=heads

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,11 +33,20 @@ export function Post() {
           <a href="">jane.design/doctorcare</a>
         </p>
         <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a>{' '}
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>{" "}
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
