a6bc22b5fbd3661a705edabd236b0e6c29de49c5
Henrick <ricknogueira1231@gmail.com>
2025-01-20 16:35:57 -0300

    #D021 - Propriedades do Post

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,45 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      name: "Henrick Nogueira",
+      avatarUrl: "https://avatars.githubusercontent.com/u/55353527?v=4",
+      role: "Web Developer",
+    },
+    publishedAt: new Date("2025-01-10 20:00:00"),
+    content: [
+      { type: "paragraph", content: "Fala galeraa" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifólio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet quasi delenitidistinctio. Doloremque quibusdam asperiores quia culpa voluptatibus hic aut necessitatibus dicta consequuntur! Rem deleniti fuga accusam usmaiores aspernatur.",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+  },
+  {
+    id: 2,
+    author: {
+      name: "Ana Cláudia Nogueira",
+      avatarUrl: "https://avatars.githubusercontent.com/u/55353527?v=4",
+      role: "Web Developer",
+    },
+    publishedAt: new Date("2025-01-03 20:00:00"),
+    content: [
+      { type: "paragraph", content: "Fala galeraa" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifólio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet quasi delenitidistinctio. Doloremque quibusdam asperiores quia culpa voluptatibus hic aut necessitatibus dicta consequuntur! Rem deleniti fuga accusam usmaiores aspernatur.",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -13,10 +52,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Henrick Nogueira"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut eros eget velit convallis elementum. Sed id velit non massa condimentum ultricies."
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                publishedAt={post.publishedAt}
+                content={post.content}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,42 +1,47 @@
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL ás HH:mm", {
+    locale: ptBR,
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar
-            src="https://avatars.githubusercontent.com/u/55353527?v=4"
-          />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Henrick Nogueira</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="11 de maio ás 08:13h" datetime="2022-05-11 08:13:30">
-          Publicado há 1h
+        <time
+          title={publishedDateFormatted}
+          datetime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifólio. Lorem ipsum dolor
-          sit amet consectetur adipisicing elit. Ea amet quasi deleniti
-          distinctio. Doloremque quibusdam asperiores quia culpa voluptatibus
-          hic aut necessitatibus dicta consequuntur! Rem deleniti fuga accusamus
-          maiores aspernatur.
-        </p>
-        <p>
-          <a href="">jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
-          <a href="">#rocketseat</a>{" "}
-        </p>
+        {content.map((line) => {
+          if (line.type == "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (line.type == "link") {
+            return <p><a href="#">{line.content}</a></p>;
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
